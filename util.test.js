54128c895acadfae29aaa319f0df73cf1a8d6f12
Fernando Warno <ferwar@hotmail.com>
2024-12-24 19:39:29 -0300

    pruebas de e2e

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const { generateText, checkAndGenerate } = require('./util');
 
 test('should output name and age', () => {
@@ -9,3 +10,23 @@ test('should generate a valid text output', () => {
     const text = checkAndGenerate('Max', 29);
     expect(text).toBe('Max (29 years old)');
 });
+
+test('should create an element with text and correct class', async () => {
+    const browser = await puppeteer.launch({
+        // headless: false,
+        headless: true,
+        // slowMo: 80,
+        // args: ['--window-size=1920,1080']
+    });
+    const page = await browser.newPage();
+    await page.goto('http://127.0.0.1:5500/index.html');
+    await page.click('input#name');
+    await page.type('input#name', 'Anna');
+    await page.click('input#age');
+    await page.type('input#age', '28');
+    await page.click('#btnAddUser');
+    const finalText = await page.$eval('.user-list', el => el.textContent);
+    expect(finalText).toBe('Anna (28 years old)');
+    // await browser.close();
+
+}, 10000); // Aumenta el timeout a 10 segundos
